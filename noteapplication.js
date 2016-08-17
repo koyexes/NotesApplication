c4b96d12c422f4fbf69f21f75670b85b95f7d4bf
koyexes <koyagabriel@gmail.com>
2016-08-17 12:19:18 +0100

    Note Application updates

--- noteapplication.js
+++ noteapplication.js
@@ -9,18 +9,39 @@ class NotesApplication{
 	}
 	
 	listNotes(){
+		var list = "";
+		
 		for (var i = 0; i < this.notes.length; i++){
-			var list = "";
-			list = list + "Note ID:" +  i + "\n";
+			
+			list = list + "Note ID:" + i + "\n";
 			list = list + this.notes[i] + "\n";
 			list = list + "By Author " +  this.author ;
-			console.log(list);
-			
+		
 		}
 		
+		console.log(list);
+	}
+	
+	getNot(note_id){
+		if(this.note.length > note_id){
+			return this.notes[note_id];
+		}
+		
+	}
+	
+	search(search_text){
+		
+	}
+	
+	delete(note_id){
+		this.notes.splice(note_id,1);
 	}
 }
 
 var newNote = new NotesApplication("Daniel Steel");
 newNote.create("hello world");
-newNote.listNotes();
+newNote.create("hello world 2");
+newNote.create("hello world 3");
+console.log(newNote.listNotes());
+newNote.delete(0);
+console.log(newNote.listNotes());
